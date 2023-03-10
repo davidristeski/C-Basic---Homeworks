8e5f52d542fd5fcb59d724c001061e9023c17443
David Risteski <davristeski@gmail.com>
2023-03-10 22:37:35 +0100

    Added new files H1

---
+++ 01-Homework/Homework #1/Homework #1/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Simple calculator with two numbers and the functions ( +, - , * , / )
+
+Console.WriteLine("This is a simple two numbers calculator.");
+Console.WriteLine("");
+Console.WriteLine("Please enter the first number");
+double inputNumberOne = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number");
+double inputNumberTwo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter one of the following operators ( +, - , * , / )");
+string inputMathSign = Console.ReadLine();
+
+double result;
+
+if (inputMathSign == "+")
+{
+    result = inputNumberOne + inputNumberTwo;
+    Console.WriteLine("The result of " + inputNumberOne + " + " + inputNumberTwo + " is " + result);
+}
+else if (inputMathSign == "-")
+{
+    result = inputNumberOne - inputNumberTwo;
+    Console.WriteLine("The result of " + inputNumberOne + " - " + inputNumberTwo + " is " + result);
+}
+else if (inputMathSign == "*")
+{
+    result = inputNumberOne * inputNumberTwo;
+    Console.WriteLine("The result of " + inputNumberOne + " * " + inputNumberTwo + " is " + result);
+}
+else if (inputMathSign == "/")
+{
+       if(inputNumberTwo == 0)
+    {
+        Console.WriteLine("Cannot divide with 0! Retry!");
+    }
+       else { 
+    result = (double)inputNumberOne / inputNumberTwo;
+    Console.WriteLine("The result of " + inputNumberOne + " / " + inputNumberTwo + " is " + result);
+    }
+}
---
+++ 01-Homework/Homework #1/Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Average number function
+
+Console.WriteLine("This is a simple average function.");
+Console.WriteLine("");
+Console.WriteLine("Please enter the first number:");
+double inputNumberOne = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number:");
+double inputNumberTwo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the third number:");
+double inputNumberThree = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the fourth number:");
+double inputNumberFour = Convert.ToDouble(Console.ReadLine());
+
+double averageResult = (inputNumberOne + inputNumberTwo + inputNumberThree + inputNumberFour) / 4;
+Console.WriteLine("The average of the following numbers: " + inputNumberOne + ", " + inputNumberTwo + ", " + inputNumberThree + ", " + inputNumberFour + " is " + averageResult + "!");
